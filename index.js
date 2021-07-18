07521981e6c82fed7ac37fc79ae4040e976bc95b
MrEmii <yosoyemiimc@gmail.com>
2021-07-18 01:24:22 -0300

    t

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ app.use(morgan("dev"));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-const tt = require("./data/dataToFirebase");
-
+//const tt = require("./data/dataToFirebase");
 
 mongo
   .connect(MongoDBSession)
