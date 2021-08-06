571c21dc5014c8473c59a8cc5bd90489cba4b072
MrEmii <yosoyemiimc@gmail.com>
2021-08-06 01:37:28 -0300

    Redis ip

--- src/db/redis.cache.js
+++ src/db/redis.cache.js
@@ -2,7 +2,7 @@ const cache = require('express-redis-cache');
 
 const cacheRedis = cache({
   client: require('redis').createClient({
-    host: 'chek-cache-001.tbawjh.0001.sae1.cache.amazonaws.com'
+    host: 'chek-cache-ro.tbawjh.ng.0001.sae1.cache.amazonaws.com'
   })
 })
 
