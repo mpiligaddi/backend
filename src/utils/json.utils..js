f3ca0d922985c38994eb48175c847ea3aa31411d
MrEmii <yosoyemiimc@gmail.com>
2021-08-17 22:46:49 -0300

    Se quita createdAt de reporte como obligatorio

--- src/utils/json.utils..js
+++ src/utils/json.utils..js
@@ -68,7 +68,7 @@ const reportSchema = (report) => {
   return {
     id: "/report",
     type: "object",
-    required: ["chainId", "clientId", "branchId", "createdAt", "isComplete", "type", "location", "categories",],
+    required: ["chainId", "clientId", "branchId", "isComplete", "type", "location", "categories",],
     properties: {
       chainId: {
         type: "string"
